4c695fd815372317212077be28be25837355544b
anekos <anekos@snca.net>
2018-04-19 20:57:23 +0900

    Add `attribute`

--- src/js/content.js
+++ src/js/content.js
@@ -6,6 +6,11 @@ function main() {
     if (message.command === 'selector') {
       let found = document.querySelector(message.query);
       return Promise.resolve(found ? found.textContent : '');
+    } else if (message.command === 'attribute') {
+      let found = document.querySelector(message.query);
+      if (found)
+        found = found.getAttribute(message.attribute);
+      return Promise.resolve(found || '');
     }
   });
 }
--- src/js/source.js
+++ src/js/source.js
@@ -3,9 +3,12 @@ export default (args) => ({
   title: context => context.tab.title,
   url: context => context.tab.url,
   date: context => DateFormat(args),
-  selector: async context => {
+  selector: context => {
     return browser.tabs.sendMessage(context.tab.id, {command: 'selector', query: args});
-  }
+  },
+  attribute: context => {
+    console.log(args);
+    let [attribute, query] = args.split(/\s+/, 2);
+    return browser.tabs.sendMessage(context.tab.id, {command: 'attribute', query, attribute});
+  },
 });
-
-
