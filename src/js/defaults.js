f3677207305866286f44127f0d7ff6aa3d7f2a03
anekos <anekos@snca.net>
2018-04-19 21:37:32 +0900

    Add `quote` to defaults

--- src/js/defaults.js
+++ src/js/defaults.js
@@ -2,6 +2,7 @@
 export default {
   formats: [
     {name: 'default', text: '⟦$(title)⟧ $(url)'},
+    {name: 'quote', text: '"$(selected)" ⟦$(title)⟧ $(url)'},
     {name: 'markdown', text: '[$(title)]($(url))'},
     {name: 'markdown-image', text: '![$(title)]($(url))'},
   ],
